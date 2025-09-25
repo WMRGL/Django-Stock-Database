a473d98030b6f873acd885e61ff8d2f2d412ba4f
Isaiah Olatunbosun <daezyworld@gmail.com>
2025-09-25 09:14:25 +0100

    user auth

--- stock_web/authentication.py
+++ stock_web/authentication.py
@@ -7,7 +7,7 @@ from stock_web.models import STAFF
 class ShireBackend(BaseBackend):
 
     def authenticate(self, request, username=None, password=None):
-        _isUserPasswordValid = False
+        isUserPasswordValid = False
         # Make sure the username and password are both provided
         if username is None or password is None:
             return None
@@ -15,24 +15,26 @@ class ShireBackend(BaseBackend):
             userValidObj = STAFF.objects.filter(STAFF_CODE=username, EMPLOYMENT_END_DATE__isnull=True)
 
             if userValidObj is None:
+                print("Shire Backend: User not found")
                 return None
             for item in userValidObj:
                 # Do the password check separate, because the SQL server comparison
                 # is not case-sensitive.  Whereas the code below is!
                 if item.PASSWORD == password:
-                    _isUserPasswordValid = True
+                    isUserPasswordValid = True
 
         except Exception:
             return None
 
         # The following logic is predicated on each user being replicated in
         # the standard Django structure (the User model class)
-        if _isUserPasswordValid:
+        if isUserPasswordValid:
             try:
                 # Try to find the user record in the auth_user table
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
                 # If not found create a new user. There's no need to set a password
+                print("Django user not found, creating new user")
                 user = User(username=username)
                 user.save()
             return user
